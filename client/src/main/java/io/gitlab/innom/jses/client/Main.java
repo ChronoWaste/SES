fd876519aecaf1a42b0cde67d3a3ee0ed3fb2eeb
Juan Marín <juanpablo.marin76@gmail.com>
2017-04-21 20:59:29 -0500

    chore(change project structure to multi-module):

---
+++ client/src/main/java/io/gitlab/innom/jses/client/Main.java
@@ -0,0 +1,9 @@
+package io.gitlab.innom.jses.client;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ server/src/main/java/io/gitlab/innom/jses/server/Main.java
@@ -0,0 +1,9 @@
+package io.gitlab.innom.jses.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
